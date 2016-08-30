0061e1441473ee06f372efab628d8eb24d6ab305
Chris <chris.wiseman74@gmail.com>
2016-08-29 21:24:12 -0700

    Adding database insertion

--- index.php
+++ index.php
@@ -10,17 +10,40 @@ include 'db.php';
 <body>
     <div id="container">
         <div id="chat_box">
+          <?php
+            $query = "SELECT * FROM chat ORDER BY id DESC"; /* THIS WILL ALLOW THE NEW MESSAGE TO APPEAR AT THE TOP */
+            $run = $con->query($query);
+            
+            while ($row = $run->fetch_array()) :/*Makes an array of all items in the table*/
+          ?>
            <div id="chat_data">
-               <span style="color:green">Chris: </span>
-               <span style="color: red">How are you? </span>
-               <span style="float-right">1:00 p.m.</span>
-           </div>    
+               <span style="color:green"><?php echo $row['name']; ?></span> :
+               <span style="color: red"><?php echo $row['message']; ?></span> 
+               <span style="float-right"><?php echo $row['date']; ?></span> 
+           </div> 
+           <?php endwhile; ?>
         </div>
     </div>
     <form method="post" action="index.php">
         <input type="text" name="name" placeholder="enter name"/><br />
-    <textarea name="enter message" placeholder="enter message"></textarea><br />
+    <textarea name="msg" placeholder="enter message"></textarea><br />
         <input type="submit" name="submit" value="Send it"/>
     </form>
+    <?php
+        if(isset($_POST['submit'])) {
+            
+        $name = $_POST['name'];    
+        $message = $_POST['msg'];
+            
+        $query = "INSERT INTO chat (name, message) values ('$name','$message')";
+            
+        $run = $con->query($query);
+        
+        if($run) {
+            echo "<embed loop='false' src='chat.wav' hidden='true' autoplay='true'>";
+        }
+            
+        }
+    ?>
 </body>
 </html>
